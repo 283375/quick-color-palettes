f8667858936db666083e6467085655130524a3f2
283375 <bilibili283375@163.com>
2022-05-01 15:50:49 +0800

    [Vite config] Fix(?) slow response of the dev server
    - Not sure, maybe the problem still exists...

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,36 @@
 import packageJson from './package.json'
-import { execSync } from 'child_process'
+import { promisify } from 'util'
+import { exec } from 'child_process'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { ViteAliases } from 'vite-aliases'
 import ViteSvgLoader from 'vite-svg-loader'
 
+const execPromise = promisify(exec)
+
+async function getGitVersion() {
+  const { stdout, stderr } = await execPromise(
+    'git describe --tags --long --always'
+  )
+  if (stderr) throw stderr
+  return stdout
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    ViteAliases({ useConfig: true, deep: false }),
-    ViteSvgLoader(),
-  ],
-  define: {
-    __PACKAGE_JSON_VER__: JSON.stringify(packageJson.version),
-    __GIT_VERSION__: JSON.stringify(
-      Buffer.from(execSync('git describe --tags --long --always'))
-        .toString()
-        .replace('\n', '')
-    ),
-  },
+export default defineConfig(async ({ command, mode }) => {
+  const __GIT_VERSION__ = await getGitVersion()
+  return {
+    plugins: [
+      vue(),
+      ViteAliases({ useConfig: true, deep: false }),
+      ViteSvgLoader(),
+    ],
+    define: {
+      __PACKAGE_JSON_VER__: JSON.stringify(packageJson.version),
+      __GIT_VERSION__: JSON.stringify(
+        Buffer.from(__GIT_VERSION__).toString().replace('\n', '')
+      ),
+    },
+  }
 })
